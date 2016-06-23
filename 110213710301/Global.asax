1a56dd454715137623e0b5519c6e212486da5bd4
alinaba <1102137103@gm.kuas.edu.tw>
2016-06-23 14:14:34 +0800

    0623

---
+++ 110213710301/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_110213710301.MvcApplication" Language="C#" %>
---
+++ 110213710301/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_110213710301.Startup))]
+namespace _110213710301
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
